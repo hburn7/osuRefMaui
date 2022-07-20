19c67a05fa25208e0088655d3a638418abf8a787
hburn7 <hburnett777@gmail.com>
2022-07-20 14:45:48 -0400

    Chat dequeue loop, cleanup

--- osuRefMaui/osuRefMaui/MissionControl.xaml.cs
+++ osuRefMaui/osuRefMaui/MissionControl.xaml.cs
@@ -1,64 +1,102 @@
+using Microsoft.Extensions.Logging;
+using osuRefMaui.Core.Derivatives.Buttons;
+using osuRefMaui.Core.IRC;
+
 namespace osuRefMaui;
 
 public partial class MissionControl : ContentPage
 {
-	public MissionControl()
+	private static bool _previouslyLoaded;
+	private readonly ChatQueue _chatQueue;
+	private readonly TabHandler _tabHandler;
+
+	public MissionControl(TabHandler tabHandler, ChatQueue chatQueue)
 	{
+		_tabHandler = tabHandler;
+		_chatQueue = chatQueue;
+
+		_previouslyLoaded = false;
+
 		InitializeComponent();
+
+		Loaded += MissionControl_Loaded;
 	}
 
-    private void BtnAddChannel_Clicked(object sender, EventArgs e)
-    {
+	private void MissionControl_Loaded(object sender, EventArgs e)
+	{
+		if (!_previouslyLoaded)
+		{
+			_tabHandler.OnTabCreated += UI_AddTab;
 
-    }
+			// Create default tab
+			_tabHandler.AddTab(TabHandler.DefaultTabName);
 
-    private void ChatBox_Completed(object sender, EventArgs e)
-    {
+			_chatQueue.OnDequeue += m =>
+			{
+				// Route chat labels to tab
+				Window.Dispatcher.Dispatch(() => { _tabHandler.RouteToTab(m); });
+			};
 
-    }
+			// Swap to default tab
+			UI_SwapTab(TabHandler.DefaultTabName);
+			
+			// Chat dequeue loop
+			Task.Run(async () =>
+			{
+				while (true)
+				{
+					/*
+			         * Every 0.25s the chat dequeue loop is run which checks for new messages.
+			         * If there is a message in the queue, it is immediately dequeued.
+			         *
+			         * Dequeuing messages fires an event that other classes can listen to
+			         * and act upon. This is how the UI gets updated.
+			         */
 
-    private void cmdMpTimer120_Clicked(object sender, EventArgs e)
-    {
+					while (_chatQueue.TryDequeue(out _)) { }
 
-    }
+					await Task.Delay(250);
+				}
+			});
+		}
 
-    private void cmdMpTimer90_Clicked(object sender, EventArgs e)
-    {
+		_previouslyLoaded = true;
+	}
 
-    }
+	private void UI_SwapTab(string channel)
+	{
+		if (!_tabHandler.TryGetChatStack(channel, out var chatStack))
+		{
+			return;
+		}
 
-    private void cmdMpStart10_Clicked(object sender, EventArgs e)
-    {
+		ChatScrollView.Content = chatStack;
+	}
 
-    }
+	private void UI_AddTab(string channel) => Window.Dispatcher.Dispatch(() =>
+	{
+		var button = new TabButton(channel);
+		button.Clicked += (s, _) => UI_SwapTab(((TabButton)s)!.Text);
 
-    private void cmdMpStart5_Clicked(object sender, EventArgs e)
-    {
+		ChatTabHorizontalStack.Insert(ChatTabHorizontalStack.Count - 1, button);
+	});
 
-    }
+	private async void BtnAddChannel_Clicked(object sender, EventArgs e)
+	{
+		string channel = await DisplayPromptAsync("Add Channel",
+			"Type the channel or username to add.", placeholder: "#osu");
 
-    private void cmdMpAbort_Clicked(object sender, EventArgs e)
-    {
+		_tabHandler.AddTab(channel);
+	}
 
-    }
-
-    private void filterCheckBoxBanchoJoin_CheckChanged(object sender, EventArgs e)
-    {
-
-    }
-
-    private void filterCheckBoxBanchoQuit_CheckChanged(object sender, EventArgs e)
-    {
-
-    }
-
-    private void filterCheckBoxBanchoPing_CheckChanged(object sender, EventArgs e)
-    {
-
-    }
-
-    private void filterCheckBoxBanchoSlotMove_CheckChanged(object sender, EventArgs e)
-    {
-
-    }
+	private void ChatBox_Completed(object sender, EventArgs e) {}
+	private void cmdMpTimer120_Clicked(object sender, EventArgs e) {}
+	private void cmdMpTimer90_Clicked(object sender, EventArgs e) {}
+	private void cmdMpStart10_Clicked(object sender, EventArgs e) {}
+	private void cmdMpStart5_Clicked(object sender, EventArgs e) {}
+	private void cmdMpAbort_Clicked(object sender, EventArgs e) {}
+	private void filterCheckBoxBanchoJoin_CheckChanged(object sender, EventArgs e) {}
+	private void filterCheckBoxBanchoQuit_CheckChanged(object sender, EventArgs e) {}
+	private void filterCheckBoxBanchoPing_CheckChanged(object sender, EventArgs e) {}
+	private void filterCheckBoxBanchoSlotMove_CheckChanged(object sender, EventArgs e) {}
 }
