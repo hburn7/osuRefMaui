0924cc5918a6baba81ab8f5812b19978121f64c5
hburn7 <hburnett777@gmail.com>
2022-07-20 14:43:41 -0400

    Cleanup

--- osuRefMaui/osuRefMaui/Core/IRC/IncomingMessageHandler.cs
+++ osuRefMaui/osuRefMaui/Core/IRC/IncomingMessageHandler.cs
@@ -1,22 +1,17 @@
 ﻿using IrcDotNet;
-using Microsoft.Extensions.Logging;
 
 namespace osuRefMaui.Core.IRC
 {
     public class IncomingMessageHandler
     {
-        private readonly StandardIrcClient _client;
-        private readonly ILogger<IncomingMessageHandler> _logger;
         private readonly ChatQueue _chatQueue;
 
-        public IncomingMessageHandler(ILogger<IncomingMessageHandler> logger,
-            StandardIrcClient client, ChatQueue chatQueue)
+        // ReSharper disable once SuggestBaseTypeForParameterInConstructor
+        public IncomingMessageHandler(StandardIrcClient client, ChatQueue chatQueue)
         {
-            _logger = logger;
             _chatQueue = chatQueue;
-            _client = client;
-
-            _client.RawMessageReceived += OnMessageReceived;
+            
+            client.RawMessageReceived += OnMessageReceived;
         }
 
         private void OnMessageReceived(object sender, IrcRawMessageEventArgs e)
