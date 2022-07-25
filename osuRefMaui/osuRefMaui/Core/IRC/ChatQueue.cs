7d176600377fc4fb720a998c33b1ffb100fbd5de
hburn7 <hburnett777@gmail.com>
2022-07-25 08:05:13 -0400

    Documentation, OnPersistentEmpty event

--- osuRefMaui/osuRefMaui/Core/IRC/ChatQueue.cs
+++ osuRefMaui/osuRefMaui/Core/IRC/ChatQueue.cs
@@ -10,8 +10,18 @@ namespace osuRefMaui.Core.IRC
 	{
 		private readonly ILogger<ChatQueue> _logger;
 		public ChatQueue(ILogger<ChatQueue> logger) { _logger = logger; }
+		/// <summary>
+		/// Fired whenever a message is enqueued
+		/// </summary>
 		public event Action<IChatMessage> OnEnqueue;
+		/// <summary>
+		/// Fired whenever a message is dequeued
+		/// </summary>
 		public event Action<IChatMessage> OnDequeue;
+		/// <summary>
+		/// Fired when the queue remains empty for 500ms after a dequeue.
+		/// </summary>
+		public event Action OnPersistentEmpty;
 
 		public void Enqueue(IChatMessage message)
 		{
@@ -26,6 +36,20 @@ namespace osuRefMaui.Core.IRC
 			if (base.TryDequeue(out chatMessage))
 			{
 				OnDequeue?.Invoke(chatMessage);
+				
+				if (IsEmpty)
+				{
+					// If the queue is empty and remains empty after 500ms, fire the OnPersistentEmpty event
+					Task.Run(async () =>
+					{
+						await Task.Delay(500);
+						if (IsEmpty)
+						{
+							OnPersistentEmpty?.Invoke();
+						}
+					});
+				}
+				
 				return true;
 			}
 
