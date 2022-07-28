71c7c7bba6a4ba8c6366a11129ba1bcd066999ab
hburn7 <hburnett777@gmail.com>
2022-07-28 19:26:51 -0400

    Cleanup

--- osuRefMaui/osuRefMaui/Core/Derivatives/Labeling/Spans/HyperlinkConsoleTextSpan.cs
+++ osuRefMaui/osuRefMaui/Core/Derivatives/Labeling/Spans/HyperlinkConsoleTextSpan.cs
@@ -7,27 +7,23 @@ namespace osuRefMaui.Core.Derivatives.Labeling.Spans
 		public static readonly BindableProperty UrlProperty =
 			BindableProperty.Create(nameof(Url), typeof(string), typeof(HyperlinkConsoleTextSpan));
 
-		public string Url
-		{
-			get => (string)GetValue(UrlProperty);
-			set => SetValue(UrlProperty, value);
-		}
-		
 		public HyperlinkConsoleTextSpan()
 		{
 			// ReSharper disable once AsyncVoidLambda
 			var cmd = new Command(async () => { await Browser.Default.OpenAsync(Url); });
 
 			TextColor = ChatPalette.HyperlinkConsoleSpanTextColor;
-			GestureRecognizers.Add(new ClickGestureRecognizer
-			{
-				Command = cmd
-			});
 
 			GestureRecognizers.Add(new TapGestureRecognizer
 			{
 				Command = cmd
 			});
+		}
+
+		public string Url
+		{
+			get => (string)GetValue(UrlProperty);
+			set => SetValue(UrlProperty, value);
 		}
 	}
 }
